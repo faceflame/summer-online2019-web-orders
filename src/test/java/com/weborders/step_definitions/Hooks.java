433dab77b91a3ab4a98307688521b61ddf3dc209
faceflame <ozcansahin0654@gmail.com>
2020-01-18 12:24:10 -0500

    login degisti

--- src/test/java/com/weborders/step_definitions/Hooks.java
+++ src/test/java/com/weborders/step_definitions/Hooks.java
@@ -2,9 +2,16 @@ package com.weborders.step_definitions;
 
 import com.weborders.utilities.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 public class Hooks {
 
+
+    @Before
+    public void setup (){
+        Driver.get().manage().window().maximize();
+
+    }
     //after hook
     //will close browser after every test
     @After
